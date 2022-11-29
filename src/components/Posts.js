5e7c1a9454c43d1226004a54178f4e86a59eb3b1
Fahd Munir <fahd.munir.89@gmail.com>
2022-11-29 17:43:37 -0500

    created routes and basic styling for crud with bootstrap

---
+++ src/components/Posts.js
@@ -0,0 +1,153 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [updatedPost, setUpdatedPost] = useState({});
+
+  const navigate = useNavigate();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    axios
+      .get("/posts")
+      .then((res) => {
+        console.log(res);
+        setPosts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const deletePost = (id) => {
+    axios
+      .delete(`/delete/${id}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    window.location.reload();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUpdatedPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const updatePost = (post) => {
+    setUpdatedPost(post);
+    handleShow();
+  };
+
+  const saveUpdatedPost = () => {
+    axios
+      .put(`/update/${updatedPost._id}`, updatedPost)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    handleClose();
+    window.location.reload();
+  };
+
+  return (
+    <div style={{ width: "90%", textAlign: "center", margin: "auto auto" }}>
+      <h1> Posts page </h1>
+      <Button
+        style={{ width: "100%", marginBottom: "1rem" }}
+        variant="outline-dark"
+        onClick={() => navigate(-1)}
+      >
+        {" "}
+        BACK{" "}
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update a post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Control
+                style={{ marginBottom: "1rem" }}
+                placeholder="title"
+                name="title"
+                value={updatedPost.title ? updatedPost.title : ""}
+                onChange={handleChange}
+              />
+              <Form.Control
+                placeholder="description"
+                name="description"
+                value={updatedPost.description ? updatedPost.description : ""}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={saveUpdatedPost}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {posts ? (
+        <>
+          {posts.map((post) => {
+            return (
+              <div
+                key={post._id}
+                style={{
+                  border: "solid lightgrey 1px",
+                  borderRadius: "8px",
+                  marginBottom: "1rem",
+                  padding: "1rem",
+                }}
+              >
+                <h4>{post.title}</h4>
+                <p> {post.description} </p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Button
+                    variant="outline-info"
+                    style={{ width: "100%", marginRight: "1rem" }}
+                    onClick={() => updatePost(post)}
+                  >
+                    UPDATE
+                  </Button>
+                  <Button
+                    variant="outline-danger"
+                    style={{ width: "100%" }}
+                    onClick={() => deletePost(post._id)}
+                  >
+                    DELETE
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default Posts;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import CreatePost from './components/CreatePost';
+import Posts from './components/Posts';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/create/posts" element={<Posts />} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
